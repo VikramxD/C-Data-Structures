4600ded83077ea64e492456d2aff512c39e24353
Vikramjeet Singh <72499426+VikramxD@users.noreply.github.com>
2020-10-31 18:10:52 +0530

    Linked List and Traversal

--- main.c
+++ main.c
@@ -1,24 +1,51 @@
-#include <stdio.h>
-void fun(int n)
+#include<stdio.h>
+#include<stdlib.h>
+//Basic Node for linked list.
+struct Node{
+  int Data;
+  struct Node*Next;
+};
+//Traversal and Printing.
+void Print_list(struct Node*n)
 {
-  if (n>0)
-  {
-    printf("%d\n",n);
-    fun(n-1);
-  }
-
+while(n!=NULL)
+{
+printf("%d\n",n->Data);
+n=n->Next;
+}
 }
 
-int main()
+//Create a five nodes
+struct Node * head=NULL;
+struct Node * second=NULL;
+struct Node * third=NULL;
+struct Node * fourth=NULL;
+struct Node * fifth=NULL;
+//Allocate the nodes 
 
-{
-int x;
-printf("Enter the number: ");
-scanf("%d",&x);
-fun(x);
+
+int main(){
+head= (struct Node *)malloc(sizeof(struct Node));
+second=(struct Node *)malloc(sizeof(struct Node));
+third= (struct Node *)malloc(sizeof(struct Node));
+fourth= (struct Node *)malloc(sizeof(struct Node));
+fifth=(struct Node *)malloc(sizeof(struct Node));
+
+head->Data=1;
+head->Next=second;
+second->Data=2;
+second->Next=third;
+third->Data=3;
+third->Next=fourth;
+fourth->Data=4;
+fourth->Next=fifth;
+fifth->Data=12;
+fifth->Next=NULL;     
+
+Print_list(head);                 
 
 return 0;
-
-
-
 }
+
+
+
